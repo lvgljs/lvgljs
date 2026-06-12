473b69f9974c02972ac36fca8faa6ecb798d5138
Yonggang Luo <luoyonggang@gmail.com>
2026-06-12 15:44:33 +0800

    refactor(style): migrate arc pipe to StyleBatch

--- src/render/react/core/style/pipe/arc.ts
+++ src/render/react/core/style/pipe/arc.ts
@@ -1,24 +1,17 @@
-import { ColorType } from "../color";
-import { ProcessBoolean, ProcessColor, ProcessPx } from "../util";
+import {
+  colorTransform,
+  NormalizePositivePx,
+  NormalizeBoolean,
+} from "../util";
+import type { ArcStyleType } from "../type";
+import { StyleTransformResult } from "../batch";
 
-const obj = {
-  "arc-width": ProcessPx,
-  "arc-color": ProcessColor,
-  "arc-rounded": ProcessBoolean,
-};
-const keys = Object.keys(obj);
-
-export type ArcStyleType = {
-  /** Value in pixels */
-  "arc-width"?: number;
-  "arc-color"?: ColorType;
-  "arc-rounded"?: boolean;
-};
-
-export function ArcStyle(style: ArcStyleType, result, compName) {
-  keys.forEach((key) => {
-    if (style[key] !== void 0) {
-      obj[key](key, style[key], result);
-    }
-  });
+export function ArcStyle(
+  style: ArcStyleType,
+  result: StyleTransformResult,
+) {
+  const batch = result.batch;
+  batch.pushStyle(style, "arc-width", NormalizePositivePx);
+  batch.pushStyle(style, "arc-color", colorTransform);
+  batch.pushStyle(style, "arc-rounded", NormalizeBoolean);
 }
