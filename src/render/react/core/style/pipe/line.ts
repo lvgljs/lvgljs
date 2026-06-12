ecf5ae5ad9266778977064e945332fe9d2ca5c8d
Yonggang Luo <luoyonggang@gmail.com>
2026-06-12 15:44:33 +0800

    refactor(style): migrate line pipe to StyleBatch

--- src/render/react/core/style/pipe/line.ts
+++ src/render/react/core/style/pipe/line.ts
@@ -1,26 +1,13 @@
-import { ColorType } from "../color";
-import { ProcessColor, ProcessPx, ProcessPxOrPercent } from "../util";
+import type { LineStyleType } from "../type";
+import { colorTransform, NormalizeBoolean, NormalizePositivePx } from "../util";
+import { StyleTransformResult } from "../batch";
 
-const obj = {
-  "line-width": ProcessPx,
-  "line-color": ProcessColor,
-};
-const keys = Object.keys(obj);
-
-export type LineStyleType = {
-  "line-width"?: number;
-  "line-color"?: ColorType;
-  "line-rounded"?: boolean;
-};
-
-export function LineStyle(style: LineStyleType, result, compName) {
-  keys.forEach((key) => {
-    if (style[key] !== void 0) {
-      obj[key](key, style[key], result);
-    }
-  });
-
-  if (style["line-rounded"]) {
-    result["line-rounded"] = Boolean(style["line-rounded"]);
-  }
+export function LineStyle(
+  style: LineStyleType,
+  result: StyleTransformResult,
+) {
+  const batch = result.batch;
+  batch.pushStyle(style, "line-width", NormalizePositivePx);
+  batch.pushStyle(style, "line-color", colorTransform);
+  batch.pushStyle(style, "line-rounded", NormalizeBoolean);
 }
